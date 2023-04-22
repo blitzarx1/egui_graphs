bbf7edb219df8fdaa1d91e35fdb7492fcc9fed95
blitzarx1 <blitzarx1@gmail.com>
2023-04-22 19:36:48 +0400

    renamed graph file

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 mod changes;
 mod elements;
-mod graph;
+mod graph_view;
 mod settings;
 mod state;
 
 pub use self::changes::{Changes, ChangesEdge, ChangesNode};
 pub use self::elements::{Edge, Elements, Node};
-pub use self::graph::GraphView;
+pub use self::graph_view::GraphView;
 pub use self::settings::Settings;
