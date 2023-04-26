3a02b12457ffc9bd4bb8d6496e7e4fc0451da83b
starlord <blitzarx1@gmail.com>
2023-04-26 22:54:20 +0400

    separated settings (#16)

--- src/settings.rs
+++ src/settings.rs
@@ -1,11 +1,5 @@
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct InteractionsSettings {
-    /// Fit graph to the screen
-    pub fit_to_screen: bool,
-
-    /// Zoom and pan
-    pub zoom_and_pan: bool,
-
+#[derive(Debug, Clone)]
+pub struct SettingsInteraction {
     /// Node dragging
     pub node_drag: bool,
 
@@ -16,14 +10,38 @@ pub struct InteractionsSettings {
     pub node_multiselect: bool,
 }
 
-impl Default for InteractionsSettings {
+impl Default for SettingsInteraction {
     fn default() -> Self {
         Self {
-            fit_to_screen: true,
-            zoom_and_pan: false,
             node_drag: false,
             node_select: false,
             node_multiselect: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SettingsNavigation {
+    /// Fit graph to the screen
+    pub fit_to_screen: bool,
+
+    /// Zoom and pan
+    pub zoom_and_pan: bool,
+
+    /// Padding around the graph when fitting to screen
+    pub screen_padding: f32,
+
+    /// Zoom step
+    pub zoom_step: f32,
+}
+
+impl Default for SettingsNavigation {
+    fn default() -> Self {
+        Self {
+            screen_padding: 0.3,
+            zoom_step: 0.1,
+            fit_to_screen: true,
+            zoom_and_pan: false,
         }
     }
 }
