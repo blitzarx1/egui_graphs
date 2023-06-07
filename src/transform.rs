e37f35a23199907bdd233a5bb69448e0f0bb7a04
starlord <blitzarx1@gmail.com>
2023-06-07 19:31:02 +0400

    Transform refactoring and new transform methods (#45)
    - [x] add transform function with custom node and edge transformers
    - [x] Transform refac

--- src/transform.rs
+++ src/transform.rs
@@ -1,21 +1,20 @@
 use crate::{Edge, Node};
 use egui::Vec2;
 use petgraph::{
-    stable_graph::{NodeIndex, StableGraph},
-    visit::{EdgeRef, IntoEdgeReferences},
+    stable_graph::{EdgeIndex, NodeIndex, StableGraph},
+    visit::IntoNodeReferences,
+    EdgeType,
 };
 use rand::Rng;
 use std::collections::HashMap;
 
-const SIDE_SIZE: f32 = 250.;
+pub const DEFAULT_SPAWN_SIZE: f32 = 250.;
 
 /// Helper function which transforms users `petgraph::StableGraph` isntance into the version required by the `GraphView` widget.
 ///
-/// The users  graph, `g`, can have any data type for nodes and edges, and can be either directed
-/// or undirected. The function creates a new StableGraph where the nodes and edges are encapsulated into
-/// Node and Edge structs respectively. Node struct contains the original node data, a randomly generated
-/// location (Vec2), and default values for color, selected and dragged attributes. The Edge struct encapsulates
-/// the original edge data from the users graph.
+/// The function creates a new StableGraph where the nodes and edges are encapsulated into
+/// Node and Edge structs respectively. New nodes and edges are created with `default_node_transform` and `default_edge_transform`
+/// functions. If you want to define custom transformation procedures (e.g. to use custom label for nodes), use `to_input_graph_custom` instead.
 ///
 /// # Arguments
 /// * `g` - A reference to a `petgraph::StableGraph`. The graph can have any data type for nodes and edges, and
@@ -29,45 +28,100 @@ const SIDE_SIZE: f32 = 250.;
 /// ```
 /// use petgraph::stable_graph::StableGraph;
 /// use egui_graphs::to_input_graph;
+/// use egui::Vec2;
 ///
-/// let mut graph: StableGraph<&str, &str> = StableGraph::new();
-/// let node1 = graph.add_node("A");
-/// let node2 = graph.add_node("B");
-/// graph.add_edge(node1, node2, "edge1");
+/// let mut user_graph: StableGraph<&str, &str> = StableGraph::new();
+/// let node1 = user_graph.add_node("A");
+/// let node2 = user_graph.add_node("B");
+/// user_graph.add_edge(node1, node2, "edge1");
 ///
-/// let new_graph = to_input_graph(&graph);
+/// let input_graph = to_input_graph(&user_graph);
+///
+/// assert_eq!(input_graph.node_count(), 2);
+/// assert_eq!(input_graph.edge_count(), 1);
+///
+/// let mut input_indices = input_graph.node_indices();
+/// let input_node_1 = input_indices.next().unwrap();
+/// let input_node_2 = input_indices.next().unwrap();
+/// assert_eq!(input_graph.node_weight(input_node_1).unwrap().data, Some("A"));
+/// assert_eq!(input_graph.node_weight(input_node_2).unwrap().data, Some("B"));
+///
+/// assert_eq!(input_graph.edge_weight(input_graph.edge_indices().next().unwrap()).unwrap().data, Some("edge1"));
+///
+/// assert_eq!(input_graph.node_weight(input_node_1).unwrap().label.clone().unwrap(), input_node_1.index().to_string());
+/// assert_eq!(input_graph.node_weight(input_node_2).unwrap().label.clone().unwrap(), input_node_2.index().to_string());
+///
+/// let loc_1 = input_graph.node_weight(input_node_1).unwrap().location;
+/// let loc_2 = input_graph.node_weight(input_node_2).unwrap().location;
+/// assert!(loc_1 != Vec2::ZERO);
+/// assert!(loc_2 != Vec2::ZERO);
 /// ```
-pub fn to_input_graph<N: Clone, E: Clone, Ty: petgraph::EdgeType>(
+pub fn to_input_graph<N: Clone, E: Clone, Ty: EdgeType>(
     g: &StableGraph<N, E, Ty>,
 ) -> StableGraph<Node<N>, Edge<E>, Ty> {
-    let mut new_graph = StableGraph::<Node<N>, Edge<E>, Ty>::default();
+    transform(g, default_node_transform, default_edge_transform)
+}
+
+/// The same as `to_input_graph`, but allows to define custom transformation procedures for nodes and edges.
+pub fn to_input_graph_custom<N: Clone, E: Clone, Ty: EdgeType>(
+    g: &StableGraph<N, E, Ty>,
+    node_transform: impl Fn(&StableGraph<N, E, Ty>, NodeIndex, &N) -> Node<N>,
+    edge_transform: impl Fn(&StableGraph<N, E, Ty>, EdgeIndex, &E) -> Edge<E>,
+) -> StableGraph<Node<N>, Edge<E>, Ty> {
+    transform(g, node_transform, edge_transform)
+}
+
+pub fn default_node_transform<N: Clone, E: Clone, Ty: EdgeType>(
+    _: &StableGraph<N, E, Ty>,
+    idx: NodeIndex,
+    data: &N,
+) -> Node<N> {
     let mut rng = rand::thread_rng();
+    let location = Vec2::new(
+        rng.gen_range(0. ..DEFAULT_SPAWN_SIZE),
+        rng.gen_range(0. ..DEFAULT_SPAWN_SIZE),
+    );
+    Node::new(location, data.clone()).with_label(idx.index().to_string())
+}
 
-    let node_mapping: HashMap<NodeIndex, NodeIndex> = g
-        .node_indices()
-        .map(|old_node_index| {
-            let old_node = &g[old_node_index];
-            let new_node = Node {
-                data: Some(old_node.clone()),
-                location: Vec2::new(rng.gen_range(0. ..SIDE_SIZE), rng.gen_range(0. ..SIDE_SIZE)),
-                ..Default::default()
-            };
-            let new_node_index = new_graph.add_node(new_node);
-            (old_node_index, new_node_index)
+pub fn default_edge_transform<N: Clone, E: Clone, Ty: EdgeType>(
+    _: &StableGraph<N, E, Ty>,
+    _: EdgeIndex,
+    data: &E,
+) -> Edge<E> {
+    Edge::new(data.clone())
+}
+
+fn transform<N: Clone, E: Clone, Ty: EdgeType>(
+    g: &StableGraph<N, E, Ty>,
+    node_transform: impl Fn(&StableGraph<N, E, Ty>, NodeIndex, &N) -> Node<N>,
+    edge_transform: impl Fn(&StableGraph<N, E, Ty>, EdgeIndex, &E) -> Edge<E>,
+) -> StableGraph<Node<N>, Edge<E>, Ty> {
+    let mut input_g = StableGraph::<Node<N>, Edge<E>, Ty>::default();
+
+    let input_by_user = g
+        .node_references()
+        .map(|(user_n_idx, user_n)| {
+            let input_n_index = input_g.add_node(node_transform(g, user_n_idx, user_n));
+            (user_n_idx, input_n_index)
         })
-        .collect();
+        .collect::<HashMap<NodeIndex, NodeIndex>>();
 
-    for edge in g.edge_references() {
-        let new_edge = Edge {
-            data: Some(edge.weight().clone()),
-            ..Default::default()
-        };
-        let source_node = *node_mapping.get(&edge.source()).unwrap();
-        let target_node = *node_mapping.get(&edge.target()).unwrap();
-        new_graph.add_edge(source_node, target_node, new_edge);
-    }
+    g.edge_indices().for_each(|user_e_idx| {
+        let (user_source_n_idx, user_target_n_idx) = g.edge_endpoints(user_e_idx).unwrap();
+        let user_e = g.edge_weight(user_e_idx).unwrap();
 
-    new_graph
+        let input_source_n = *input_by_user.get(&user_source_n_idx).unwrap();
+        let input_target_n = *input_by_user.get(&user_target_n_idx).unwrap();
+
+        input_g.add_edge(
+            input_source_n,
+            input_target_n,
+            edge_transform(g, user_e_idx, user_e),
+        );
+    });
+
+    input_g
 }
 
 #[cfg(test)]
@@ -78,59 +132,61 @@ mod tests {
 
     #[test]
     fn test_to_input_graph_directed() {
-        let mut old_graph: StableGraph<_, _, Directed> = StableGraph::new();
-        let n1 = old_graph.add_node("Node1");
-        let n2 = old_graph.add_node("Node2");
-        old_graph.add_edge(n1, n2, "Edge1");
+        let mut user_g: StableGraph<_, _, Directed> = StableGraph::new();
+        let n1 = user_g.add_node("Node1");
+        let n2 = user_g.add_node("Node2");
+        user_g.add_edge(n1, n2, "Edge1");
 
-        let new_graph = to_input_graph(&old_graph);
+        let input_g = to_input_graph(&user_g);
 
-        assert_eq!(old_graph.node_count(), new_graph.node_count());
-        assert_eq!(old_graph.edge_count(), new_graph.edge_count());
+        assert_eq!(user_g.node_count(), input_g.node_count());
+        assert_eq!(user_g.edge_count(), input_g.edge_count());
+        assert_eq!(user_g.is_directed(), input_g.is_directed());
 
-        for (old_node_index, new_node_index) in
-            new_graph.node_indices().zip(old_graph.node_indices())
-        {
-            let old_node = old_graph.node_weight(old_node_index).unwrap();
-            let new_node = new_graph.node_weight(new_node_index).unwrap();
+        for (user_idx, input_idx) in input_g.node_indices().zip(user_g.node_indices()) {
+            let user_n = user_g.node_weight(user_idx).unwrap();
+            let input_n = input_g.node_weight(input_idx).unwrap();
 
-            assert_eq!(new_node.data, Some(old_node.clone()));
+            assert_eq!(input_n.data, Some(*user_n));
 
-            assert!(new_node.location.x >= 0.0 && new_node.location.x <= SIDE_SIZE);
-            assert!(new_node.location.y >= 0.0 && new_node.location.y <= SIDE_SIZE);
+            assert!(input_n.location.x >= 0.0 && input_n.location.x <= DEFAULT_SPAWN_SIZE);
+            assert!(input_n.location.y >= 0.0 && input_n.location.y <= DEFAULT_SPAWN_SIZE);
 
-            assert_eq!(new_node.color, None);
-            assert_eq!(new_node.selected, false);
-            assert_eq!(new_node.dragged, false);
+            assert_eq!(input_n.label.clone().unwrap(), user_idx.index().to_string());
+
+            assert_eq!(input_n.color, None);
+            assert!(!input_n.selected);
+            assert!(!input_n.dragged);
         }
     }
 
     #[test]
     fn test_to_input_graph_undirected() {
-        let mut old_graph: StableGraph<_, _, Undirected> = StableGraph::default();
-        let n1 = old_graph.add_node("Node1");
-        let n2 = old_graph.add_node("Node2");
-        old_graph.add_edge(n1, n2, "Edge1");
+        let mut user_g: StableGraph<_, _, Undirected> = StableGraph::default();
+        let n1 = user_g.add_node("Node1");
+        let n2 = user_g.add_node("Node2");
+        user_g.add_edge(n1, n2, "Edge1");
 
-        let new_graph = to_input_graph(&old_graph);
+        let input_g = to_input_graph(&user_g);
 
-        assert_eq!(old_graph.node_count(), new_graph.node_count());
-        assert_eq!(old_graph.edge_count(), new_graph.edge_count());
+        assert_eq!(user_g.node_count(), input_g.node_count());
+        assert_eq!(user_g.edge_count(), input_g.edge_count());
+        assert_eq!(user_g.is_directed(), input_g.is_directed());
 
-        for (old_node_index, new_node_index) in
-            new_graph.node_indices().zip(old_graph.node_indices())
-        {
-            let old_node = old_graph.node_weight(old_node_index).unwrap();
-            let new_node = new_graph.node_weight(new_node_index).unwrap();
+        for (user_idx, input_idx) in input_g.node_indices().zip(user_g.node_indices()) {
+            let user_n = user_g.node_weight(user_idx).unwrap();
+            let input_n = input_g.node_weight(input_idx).unwrap();
 
-            assert_eq!(new_node.data, Some(old_node.clone()));
+            assert_eq!(input_n.data, Some(*user_n));
 
-            assert!(new_node.location.x >= 0.0 && new_node.location.x <= SIDE_SIZE);
-            assert!(new_node.location.y >= 0.0 && new_node.location.y <= SIDE_SIZE);
+            assert!(input_n.location.x >= 0.0 && input_n.location.x <= DEFAULT_SPAWN_SIZE);
+            assert!(input_n.location.y >= 0.0 && input_n.location.y <= DEFAULT_SPAWN_SIZE);
 
-            assert_eq!(new_node.color, None);
-            assert_eq!(new_node.selected, false);
-            assert_eq!(new_node.dragged, false);
+            assert_eq!(input_n.label.clone().unwrap(), user_idx.index().to_string());
+
+            assert_eq!(input_n.color, None);
+            assert!(!input_n.selected);
+            assert!(!input_n.dragged);
         }
     }
 }
