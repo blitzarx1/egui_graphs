6e8e715ec3676f01f6477a5415007e6ef0d1a26f
blitzarx1 <blitzarx1@gmail.com>
2023-09-28 00:04:22 +0400

    refactored style

--- src/elements/node_style.rs
+++ src/elements/node_style.rs
@@ -33,8 +33,6 @@ impl Default for StyleNodeColors {
 #[derive(Clone, Debug)]
 pub struct StyleNodeInteraction {
     pub selection: Color32,
-    pub selection_child: Color32,
-    pub selection_parent: Color32,
     pub drag: Color32,
 }
 
@@ -42,8 +40,6 @@ impl Default for StyleNodeInteraction {
     fn default() -> Self {
         Self {
             selection: Color32::from_rgba_unmultiplied(0, 255, 127, 153), // Spring Green
-            selection_child: Color32::from_rgba_unmultiplied(100, 149, 237, 153), // Cornflower Blue
-            selection_parent: Color32::from_rgba_unmultiplied(255, 105, 180, 153), // Hot Pink
             drag: Color32::from_rgba_unmultiplied(240, 128, 128, 153),    // Light Coral
         }
     }
