287ffd04aac9fe6094a7e04fbe3f9a77586025ef
Dmytro Banin <dima_banin@hotmail.com>
2023-11-18 16:22:56 -0800

    basic bevy example working

--- examples/bevy/src/main.rs
+++ examples/bevy/src/main.rs
@@ -1,24 +1,12 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-use eframe::{run_native, App, CreationContext};
-use egui::Context;
 use egui_graphs::{DefaultEdgeShape, DefaultNodeShape, Graph, GraphView};
 use petgraph::stable_graph::StableGraph;
 
 fn main() {
-    // let native_options = eframe::NativeOptions::default();
-    // run_native(
-    //     "egui_graphs_basic_demo",
-    //     native_options,
-    //     Box::new(|cc| Box::new(BasicApp::new(cc))),
-    // )
-    // .unwrap();
-
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
-        // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
-        // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
         .add_systems(Startup, setup)
         .add_systems(Update, update_graph)
         .run();
@@ -49,12 +37,13 @@ fn generate_graph() -> StableGraph<(), ()> {
 }
 
 fn setup(mut commands: Commands) {
+    // add an entity with an egui_graphs::Graph component
     commands.spawn(BasicGraph::new());
 }
 
 fn update_graph(mut contexts: EguiContexts, mut q_graph: Query<&mut BasicGraph>) {
     let ctx = contexts.ctx_mut();
-    let mut graph = q_graph.single_mut().unwrap();
+    let mut graph = q_graph.single_mut();
 
     egui::CentralPanel::default().show(ctx, |ui| {
         ui.add(&mut GraphView::<
