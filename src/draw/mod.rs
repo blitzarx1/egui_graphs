765d9eff4883e9428db6c38ea2adfa96a8c1f0d6
Dmitrii Samsonov <blitzar90@gmail.com>
2025-08-10 23:17:09 +0300

    limit visibility of drawer

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -1,8 +1,8 @@
 mod displays;
 mod displays_default;
-mod drawer;
+pub(crate) mod drawer;
 
 pub use displays::{DisplayEdge, DisplayNode};
 pub use displays_default::DefaultEdgeShape;
 pub use displays_default::DefaultNodeShape;
-pub use drawer::{DrawContext, Drawer};
+pub use drawer::DrawContext;
